85f2da881c6814c2c11af15dc747c8145f1ba7ef
Adar Sönmez <adarsonmez@outlook.com>
2023-03-15 13:31:29 +0300

    Added static files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptates, quod, quia,
+      voluptate quae voluptatem quibusdam consequuntur quos voluptatum quas quidem. Quisquam, quae.
+      Quisquam voluptates, quod, quia, voluptate quae voluptatem quibusdam consequuntur quos
+      voluptatum quas quidem. Quisquam, quae.
+    </p>
+  </main>
+</template>
